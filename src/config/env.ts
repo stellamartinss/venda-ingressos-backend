b143ac8d9d8beae8844fee7649066b79cf71537a
stellaraintree <stella.ribeiro@raintreeinc.com>
2025-12-01 10:54:58 -0300

    update env variable

--- src/config/env.ts
+++ src/config/env.ts
@@ -11,7 +11,7 @@ export const config = {
   port: Number(process.env.PORT || 4000),
   jwtSecret: required(process.env.JWT_SECRET, 'JWT_SECRET'),
   platformFee: Number(process.env.PLATFORM_FEE || 2.0),
-  databaseUrl: required(process.env.DATABASE_URL, 'DATABASE_URL'),
+  databaseUrl: required(process.env.NETLIFY_DATABASE_URL, 'NETLIFY_DATABASE_URL'),
 };
 
 
